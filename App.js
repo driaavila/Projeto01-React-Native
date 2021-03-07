b6939822e3a0d4256c5a3cf0d97a6c2f6ae38a24
Adriana Ávila <avilaapadriana@gmail.com>
2021-03-07 20:19:44 -0300

    Semana 2

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ColorPropType, StyleSheet, Text, View } from 'react-native';
-import Meucomponente from './componentes/meucomponente';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{color:'red'}}>Hello World!</Text>
-      <Text style={styles.outrosEStilos}>React Native</Text>
-      <Meucomponente name="Adriana"></Meucomponente>
-      <StatusBar style="auto" />
+    <View style={{flex:1, flexWrap:'wrap', justifyContent:'center', flexDirection:'column', alignItems: 'flex-start' }}>
+      <View style={{width:50, height:50, backgroundColor: 'red'}}></View>
+      <View style={{flexGrow: 1, width:50, height:50, backgroundColor: 'blue'}}></View>
+      <View style={{flexShrink: 0, width:50, height:50, backgroundColor: 'green'}}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  outrosEStilos: {
-    backgroundColor: '#ff2',
-    color: 'blue',
-  }
-});
